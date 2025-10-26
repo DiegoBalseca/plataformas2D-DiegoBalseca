9e3e308ef726ef5c0796acb8c46e33df7accf97d
DiegoBalseca <dbalseca@ctsformacion.com>
2025-10-26 02:38:15 +0200

    commit

--- Assets/scripts/AudioManager.cs
+++ Assets/scripts/AudioManager.cs
@@ -2,40 +2,55 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager instance{get; private set;}
+    public static AudioManager instance { get; private set; }
 
     [SerializeField] private AudioSource _bgmSource;
     [SerializeField] private AudioSource _sfxSource;
+    [SerializeField] private AudioClip _levelBGM;
 
     public AudioClip menuBGM;
 
     public AudioClip _starSFX;
     public AudioClip _coinSFX;
+    public AudioClip _heartSFX;
+    public AudioClip jumpSFX;
 
     void Awake()
     {
-        if (instance != null & instance != this)
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
         }
         else
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
-    
-    
+
+    private void Start()
+    {
+        // Reproducir automáticamente la música del nivel al iniciar
+        if (_levelBGM != null)
+        {
+            _bgmSource.clip = _levelBGM;
+            _bgmSource.loop = true;  // Asegura que se repita
+            _bgmSource.Play();
+        }
+    }
 
     public void ReproduceSound(AudioClip clip)
     {
-        _sfxSource.PlayOneShot(clip);
+        if (clip != null)
+            _sfxSource.PlayOneShot(clip);
     }
-
 
     public void ChangeBGM(AudioClip bgmclip)
     {
+        if (bgmclip == null) return;
+
         _bgmSource.clip = bgmclip;
+        _bgmSource.loop = true;
         _bgmSource.Play();
     }
-  
 }
